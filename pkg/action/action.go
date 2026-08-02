52e76ea5eaaa0a45584c95d11693865a517cf90f
sanglantes <101125878+sanglantes@users.noreply.github.com>
2026-08-02 23:01:00 +0200

    fix: improve priority duplication fix.

--- pkg/action/action.go
+++ pkg/action/action.go
@@ -30,6 +30,7 @@ func NewRegistry(eventName string, eb event.EventBus) *Registry {
 		registryPriority: make(map[int][]Registration),
 		registryName:     make(map[string]Registration),
 		eventName:        eventName,
+		eventBus:         eb,
 	}
 
 	eb.Subscribe(eventName, r.RunAll)
@@ -39,11 +40,15 @@ func NewRegistry(eventName string, eb event.EventBus) *Registry {
 
 // Register registers actions to be fired when the ntfy.msg event is triggered.
 func (r *Registry) Register(in Registration) {
+	if _, exists := r.registryPriority[in.Priority]; !exists {
+		r.priorities = append(r.priorities, in.Priority)
+		slices.SortFunc(r.priorities, func(a, b int) int {
+			return cmp.Compare(b, a)
+		})
+	}
+
 	r.registryPriority[in.Priority] = append(r.registryPriority[in.Priority], in)
 	r.registryName[in.Name] = in
-
-	r.priorities = append(r.priorities, in.Priority)
-	slices.SortFunc(r.priorities, func(a, b int) int { return cmp.Compare(b, a) })
 }
 
 func (r *Registry) Run(name string, e event.Event) error {
@@ -69,6 +74,5 @@ func (r *Registry) RunAll(e event.Event) {
 				go v.Action(e, r.eventBus)
 			}
 		}
-		break
 	}
 }
